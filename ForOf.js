9d5a79f38979649875084d927d2edf1e9ff05d9c
qkrvudtn98 <ps3624@naver.com>
2022-05-11 14:07:44 +0900

    For...Of and Maxinteger

---
+++ ForOf.js
@@ -0,0 +1,7 @@
+/** for...of 
+ * for...of 명령문은 반복가능한 객체 (Array, Map, Set, String, TypedArray, arguments)에 대하여 반복
+ * -> 정해놓은 규칙에 따라 코드 실행하는 반복문 
+ * 구문 : for(변수 of 이터러블) {
+ *          statement
+ *        } 
+ */
---
+++ Number.MAX_SAFE_INTEGER.js
@@ -0,0 +1,3 @@
+/** Number.MAX_SAFR_INTEGER
+ * Number.MAX_SAFR_INTEGER 상수는 JavaScript에서 안전한 최대 정수값을 나타낸다 (2의 53제곱 -1)
+ */
